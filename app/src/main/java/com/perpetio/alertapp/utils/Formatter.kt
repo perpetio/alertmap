371590588a08f86314edd9d3ab73f33207f00b8d
Andrjuxa <andrjuxa.ac@gmail.com>
2022-05-24 13:36:10 +0300

    Remove extra classes

--- app/src/main/java/com/perpetio/alertapp/utils/Formatter.kt
+++ app/src/main/java/com/perpetio/alertapp/utils/Formatter.kt
@@ -9,15 +9,4 @@ object Formatter {
             "dd MMMM, HH:mm", Locale.getDefault()
         ).format(dateTime)
     }
-
-    fun getShortFormat(time: String): String {
-        return try {
-            val dateTime = SimpleDateFormat(
-                "yyyy-MM-dd'T'HH:mm:ss.S'Z'", Locale.getDefault()
-            ).parse(time)!!
-            getShortFormat(dateTime)
-        } catch (e: Exception) {
-            ""
-        }
-    }
 }
