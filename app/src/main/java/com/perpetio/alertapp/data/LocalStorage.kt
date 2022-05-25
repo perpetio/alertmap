366e010a0c7630ee80d4a4ba3ef9d1f62b484625
Andrjuxa <andrjuxa.ac@gmail.com>
2022-05-25 19:52:32 +0300

    Add notification settings to storage

--- app/src/main/java/com/perpetio/alertapp/data/LocalStorage.kt
+++ app/src/main/java/com/perpetio/alertapp/data/LocalStorage.kt
@@ -2,6 +2,8 @@ package com.perpetio.alertapp.data
 
 import android.content.Context
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import com.google.gson.reflect.TypeToken
 
 class LocalStorage(
     context: Context,
@@ -35,10 +37,41 @@ class LocalStorage(
             ).apply()
         }
 
+    var shouldNotify: Boolean
+        get() = prefs.getBoolean(SHOULD_NOTIFY, false)
+        set(value) = prefs.edit().putBoolean(
+            SHOULD_NOTIFY, value
+        ).apply()
+
+    var notifyWithSound: Boolean
+        get() = prefs.getBoolean(NOTIFY_WITH_SOUND, false)
+        set(value) = prefs.edit().putBoolean(
+            NOTIFY_WITH_SOUND, value
+        ).apply()
+
+    var observedTerritories: List<String>
+        get() {
+            val json = prefs.getString(OBSERVED_TERRITORIES, "")
+            val listType = object : TypeToken<List<String>>() {}.type
+            return try {
+                gson.fromJson(json, listType)
+            } catch (e: JsonSyntaxException) {
+                emptyList()
+            }
+        }
+        set(value) {
+            val json = gson.toJson(value)
+            prefs.edit().putString(OBSERVED_TERRITORIES, json).apply()
+        }
+
     companion object {
-        private const val STORAGE_NAME = "AlertAppStorage"
+        private const val STORAGE_NAME = "alert_app_storage"
         private const val REPEAT_INTERVAL = "repeat_interval"
         private const val NEXT_UPDATE_TIME = "next_update_time"
+
+        private const val SHOULD_NOTIFY = "should_notify"
+        private const val NOTIFY_WITH_SOUND = "notify_with_sound"
+        private const val OBSERVED_TERRITORIES = "observed_territories"
 
         private const val NULL_INT = -1
         private const val NULL_LONG = -1L
