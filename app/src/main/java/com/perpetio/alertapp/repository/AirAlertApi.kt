4ce27ad99ba82704b66fe05c7e68be9379428689
Andrjuxa <andrjuxa.ac@gmail.com>
2023-02-17 09:48:20 +0200

    Add logging interceptor

--- app/src/main/java/com/perpetio/alertapp/repository/AirAlertApi.kt
+++ app/src/main/java/com/perpetio/alertapp/repository/AirAlertApi.kt
@@ -1,8 +1,10 @@
 package com.perpetio.alertapp.repository
 
+import com.perpetio.alertapp.BuildConfig
 import com.perpetio.alertapp.data_models.StatesInfoModel
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,15 +14,22 @@ import retrofit2.http.GET
 private const val BASE_URL = "https://alerts.com.ua/"
 
 private val apiService: AirAlertApi by lazy {
-    val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor { chain ->
+
+    val loggingInterceptor = HttpLoggingInterceptor()
+    loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+    val okHttpClient = OkHttpClient.Builder().apply {
+        if (BuildConfig.DEBUG) {
+            addInterceptor(loggingInterceptor)
+        }
+        addInterceptor { chain ->
             val request: Request =
                 chain.request().newBuilder().addHeader(
                     API_KEY_HEADER, API_KEY
                 ).build()
             chain.proceed(request)
         }
-        .build()
+    }.build()
 
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
